c44e50db0dc1fdf3b8ac24bb9fcef1a8c3d8592d
hotronghai <23207050@student.hcmus.edu.vn>
2024-01-13 22:47:00 +0700

    bai tap 45 python

--- 45.py
+++ 45.py
@@ -10,8 +10,8 @@ def read_video():
  		return video
 
 def print_video(video):
- 		print("Video title: ", video.title)
- 		print("Video link: ", video.link)
+ 		print("Video title: ", video.title, end="")
+ 		print("Video link: ", video.link, end="")
 
 def read_videos():
 	videos = []
@@ -23,6 +23,7 @@ def read_videos():
 	return videos
 
 def print_videos(videos):
+	print("----")
 	for i in range(len(videos)):
 		print_video(videos[i])
 
@@ -30,17 +31,32 @@ def write_video_txt(video, file):
 	file.write(video.title + "\n")
 	file.write(video.link + "\n")
 
-def write_to_txt(videos):
+def write_videos_txt(videos):
 	total = len(videos)
 	with open("data.txt", "w") as file:
 		file.write(str(total) + "\n")
 		for i in range(total):
 			write_video_txt(videos[i], file)
-			
+
+def read_video_from_txt(file):
+	title = file.readline()
+	link = file.readline()
+	video = Video(title, link)
+	return video
+
+def read_videos_from_txt():
+	videos = []
+	with open("data.txt", "r") as file:
+		total = file.readline()
+		for i in range(int(total)):
+			video = read_video_from_txt(file)
+			videos.append(video)
+	return videos
+
 def main():
  	videos = read_videos()
- 	write_to_txt(videos)
- 	print("----")
+ 	write_videos_txt(videos)
+ 	videos = read_videos_from_txt()
  	print_videos(videos)
 
-main()
+main()		
